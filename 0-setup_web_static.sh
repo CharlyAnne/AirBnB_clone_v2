fed3935cf92973805798fbf92042e590e3f4f95f
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-04-07 20:49:36 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # a script to setup my web servers for the deployment of web_static
 
-# update packages and install nginx
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
@@ -23,10 +22,10 @@ then
 fi
 
 # creating a symbolink link  linked to the folder
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the file content
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
