058896cb1e6136921a4586b9e615e6ddfd368bee
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-04-07 20:43:29 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install nginx
 # creating paths
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/released/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
