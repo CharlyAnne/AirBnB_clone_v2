4e4bb1600fdf7b10ed9cd2929c4540cb6c7e8251
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-04-07 01:29:48 +0100

    cleans

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """ Function that deploys """
 from fabric.api import *
-
+import os
 
 env.hosts = ["34.234.204.233", "34.232.69.249"]
 env.user = "ubuntu"
 
 
 def do_clean(number=0):
+   """Delete out-of-date archives.
+   
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
     """
-    cleans
-    """
-
     number = int(number)
 
     if number == 0:
