9c9d3439f416b6d4b7e83f43d7f2cc00be884d9c
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-04-08 22:22:53 +0100

    keeping it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,21 +8,22 @@ env.user = "ubuntu"
 
 
 def do_clean(number=0):
-   """Delete out-of-date archives.
-   
+    """Delete out-of-date archives.
     Args:
         number (int): The number of archives to keep.
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = int(number)
+    number = 1 if int(number) == 0 else int(number)
 
-    if number == 0:
-        numbers = 1
-    else:
-        numbers = number
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-    local('cd versions ; ls -t | head -n -{} | xargs rm -rf'.format(numbers))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | head -n -{} | xargs rm -rf'.format(path, numbers))
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
