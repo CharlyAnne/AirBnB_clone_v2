8bc5c5e3a724e6cf800b4467ca852454500a4648
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-04-10 01:28:30 +0100

    Delete testfile11.py

--- testfile11.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# Fabric script that generates a .tgz archive from the content of webstatic
-# folder of the AirBnB clone repo using do_pack
-
-from fabric.api import local
-
-
-def list_l():
-    local("ls -l")
