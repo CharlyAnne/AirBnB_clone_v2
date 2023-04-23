bcca0494376746b5be77328add8c867787758d6d
CharlyAnne <icharlieanne@gmial.com>
2023-04-23 16:54:11 +0000

    update file

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,30 +10,27 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def cities_by_states():
-    """
-    Displays list of all cities by State objects in DBStorage.
-    """
-    states = storage.all(State)
-    return render_template("8-cities_by_states.html", states=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """
-    Displays page with info about <id>, if it exists."""
-    for state in storage.all(State).values():
-        if state.id == id:
-            return render_template("9-states.html", state=state, mode='id')
-    return render_template("9-states.html", states=states)
-
-
 @app.teardown_appcontext
-def close(exception):
-    """Remove the current session"""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/states', strict_slashes=False)
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_by_id(id):
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
