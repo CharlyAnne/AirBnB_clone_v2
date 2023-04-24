d122b2a84082045eee720244ead506a96a2b2c21
Charly <100117263+CharlyAnne@users.noreply.github.com>
2023-04-24 11:07:44 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,16 +4,6 @@ import os
 
 type_storage = os.getenv('HBNB_TYPE_STORAGE')
 
-
-if type_storage == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-storage.reload()
-
-
 def storage_mode():
     """Define storage mode based on os.environ"""
     st_mode = os.getenv('HBNB_TYPE_STORAGE', default='file')
@@ -24,11 +14,6 @@ def storage_mode():
             return True
         return False
     return is_db()
-
-
-if __name__ == '__main__':
-    storage_mode()
-
 if not storage_mode():
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
@@ -37,3 +22,18 @@ else:
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
+    
+
+if type_storage == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
+
+
+if __name__ == '__main__':
+    storage_mode()
+
+
