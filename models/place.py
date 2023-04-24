50c72844a36e313afa4163a6eb1a895a06d8385c
Charly <100117263+CharlyAnne@users.noreply.github.com>
2023-04-24 11:08:36 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -8,7 +8,7 @@ from models import storage_mode
 from models.review import Review
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-place_amenity = Table('place_amenity',
+  place_amenity = Table('place_amenity',
                       Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
