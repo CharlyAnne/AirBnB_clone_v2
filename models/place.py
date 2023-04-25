ab68f9554e9d03d8835987e97f7edaa6dcc53bb4
Charly <100117263+CharlyAnne@users.noreply.github.com>
2023-04-25 22:54:01 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -10,7 +10,7 @@ place_amenity = Table("place_amenity", Base.metadata, Column(
         "place_id", String(60), ForeignKey('places.id')), Column(
         "amenity_id", String(60, collation='latin1_swedish_ci'),
         ForeignKey('amenities.id'), primary_key=True, nullable=False),
-        extend_existing=True)
+        extend_existing=True, mysql_charset="latin1")
 
 
 class Place(BaseModel, Base):
@@ -33,6 +33,9 @@ class Place(BaseModel, Base):
             'Amenity',
             secondary='place_amenity',
             viewonly=False)
+    __table_args__ = _
+            {'mysql_default_charset': 'latin1'}
+             )
 
     @property
     def reviews(self):
